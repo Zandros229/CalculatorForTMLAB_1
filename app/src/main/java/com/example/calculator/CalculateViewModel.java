9dfed07743007f1b6ca6d9168b23a0886be7fb3a
Zandros229 <ernest.figura548@gmail.com>
2019-03-31 13:56:46 +0200

    Add few Method in CalculateViewModel

--- app/src/main/java/com/example/calculator/CalculateViewModel.java
+++ app/src/main/java/com/example/calculator/CalculateViewModel.java
@@ -1,4 +1,49 @@
 package com.example.calculator;
 
-public class CalculateViewModel {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class CalculateViewModel extends ViewModel {
+    private MutableLiveData<Double> resoult;
+    private Double last_resoult;
+
+    public MutableLiveData<Double> getCurrentResoult() {
+        if (resoult == null) {
+            resoult = new MutableLiveData<Double>();
+            resoult.setValue(0.0);
+        }
+        return resoult;
+    }
+
+    public void add(double number){
+        if(resoult!=null) {
+            double temp = resoult.getValue() + number;
+            resoult.setValue(temp);
+        }
+        else
+            resoult.setValue(number);
+    }
+
+    public void sub(double number){
+        if(resoult!=null) {
+            double temp = resoult.getValue() - number;
+            resoult.setValue(temp);
+        }
+        else
+            resoult.setValue(-number);
+    }
+
+    public void multiplication(double number){
+        if(resoult!=null) {
+            double temp = resoult.getValue() * number;
+            resoult.setValue(temp);
+        }
+    }
+
+    public void division(double number){
+        if(resoult!=null) {
+            double temp = resoult.getValue() * number;
+            resoult.setValue(temp);
+        }
+    }
 }
